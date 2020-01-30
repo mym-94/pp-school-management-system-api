b533f22119305cdca4afffa3fd87f698e7c3548a
mahmoud <myahia@uplandsoftware.com>
2020-01-30 19:00:33 +0200

    SMS-11 | initial commit 1

---
+++ pp-sms-api/src/main/java/com/sms/SchoolManagementSysemApplication.java
@@ -0,0 +1,13 @@
+package com.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSysemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSysemApplication.class, args);
+	}
+
+}
